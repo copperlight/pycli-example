8147b1f180d0e229872c2ba39d587469c2c50943
Matthew Johnson <mjjohnson33@gmail.com>
2021-10-01 11:34:20 -0700

    add license file to python package

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0',
     description='Minimal example of CLI application packaging for Python.',
     url='https://github.com/copperlight/pycli-example',
+    license_files=('LICENSE',),
     python_requires='>=3.6',
     install_requires=[
         'requests'
